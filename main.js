fb68b4ba36ba466a18da06dcf2e3859b0b9b7862
julien-eric <julieneric11@gmail.com>
2017-11-14 11:05:07 -0500

    appDir points to main app directory and settings check for default value

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 const path = require('path');
+var appDir = path.dirname(require.main.filename);
 
 var Settings = function (localPath) {
   if (localPath) {
-    this.localSettings = localPath;
+    this.localSettings = require(localPath);
   } else {
-    this.localSettings = path.join(__dirname, 'settings.json');
+    this.localSettings = require(path.join(appDir,'..', 'settings.json'));
   }
 };
 
 Settings.prototype.get = function (settingName) {
   if (process.env[settingName]) {
     return process.env[settingName];
-  } else if (this.localSettings[settingName]) {
-    return this.localSettings[settingName]
+  } else if (this.localSettings[settingName] && this.localSettings[settingName].default) {
+    return this.localSettings[settingName].default
   } else {
     return undefined;
   }
