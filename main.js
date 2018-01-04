69baa294f0a9d28329a9a95f35d39f8d85abd689
julien-eric <julieneric11@gmail.com>
2018-01-04 15:58:32 -0500

    Allows for settings without default value and added set value

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const path = require('path');
 var appDir = path.dirname(require.main.filename);
 
-var Settings = function (localPath) {
+var Settings = function(localPath) {
   if (localPath) {
     this.localSettings = require(localPath);
   } else {
-    this.localSettings = require(path.join(appDir,'..', 'settings.json'));
+    this.localSettings = require(path.join(appDir, '..', 'settings.json'));
   }
 };
 
-Settings.prototype.get = function (settingName) {
+Settings.prototype.set = function(settingName) {
+  this.localSettings[settingName] = {value: settingName}
+};
+
+Settings.prototype.get = function(settingName) {
   if (process.env[settingName]) {
     return process.env[settingName];
+  } else if (this.localSettings[settingName] && this.localSettings[settingName].value) {
+    return this.localSettings[settingName].value
   } else if (this.localSettings[settingName] && this.localSettings[settingName].default) {
     return this.localSettings[settingName].default
+  } else if (this.localSettings[settingName] &&
+    !this.localSettings[settingName].default &&
+    !this.localSettings[settingName].value) {
+    return this.localSettings[settingName]
   } else {
     return undefined;
   }
